c87760fb6d798da268a64088e7587cd5b7f86031
GustavoHerreroNunes <ghnunes2010@gmail.com>
2024-06-26 16:17:48 -0300

    Fix: switched from import assertion to import attribute

--- js/dataController.js
+++ js/dataController.js
@@ -1,4 +1,4 @@
-import data from '../data.json' assert {type: 'json'};
+import data from '../data.json' with { type: 'json' };
 import projectListItem from './components/projectListItem.js';
 
 const dataController = {
